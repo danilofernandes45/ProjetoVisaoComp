91b38b4b278243d06c58c084e754d88643d6098d
danilofernandes45 <danilofernandes45@gmail.com>
2020-01-17 18:32:40 -0300

    Images

--- image_classifier_bow.py
+++ image_classifier_bow.py
@@ -1,4 +1,6 @@
 import cv2
+from sklearn.cluster import KMeans
+from sklearn.neighbors import NearestNeighbors
 
 # defining feature extractor that we want to use
 extractor = cv2.xfeatures2d.SIFT_create()
@@ -7,25 +9,31 @@ def features(image, extractor):
     keypoints, descriptors = extractor.detectAndCompute(image, None)
     return keypoints, descriptors
 
-from sklearn.cluster import KMeans
+descriptor_list = []
+for i in range(1, 41):
+    image = cv2.imread("Dataset/Full/img"+str(i)+".jpg", 0)
+    descriptor_list.append( features(image, extractor)[1] )
+for i in range(1, 41):
+    image = cv2.imread("Dataset/Empty/img"+str(i)+".jpg", 0)
+    descriptor_list.append( features(image, extractor)[1] )
 
-kmeans = KMeans(n_clusters = 800)
+kmeans = KMeans(n_clusters = 100)
 kmeans.fit(descriptor_list)
 
+print(kmeans[1])
+
 preprocessed_image = []
-for image in images:
-    image = gray(image)
-    keypoint, descriptor = features(image, extractor)
+for descriptor in descriptor_list:
     if (descriptor is not None):
         histogram = build_histogram(descriptor, kmeans)
         preprocessed_image.append(histogram)
 
-from sklearn.neighbors import NearestNeighbors
 
-data = cv2.imread(image_path)
-data = gray(data)
-keypoint, descriptor = features(data, extractor)
-histogram = build_histogram(descriptor, kmeans)
-neighbor = NearestNeighbors(n_neighbors = 20)
-neighbor.fit(preprocess_image)
-dist, result = neighbor.kneighbors([histogram])
+
+# data = cv2.imread(image_path)
+# data = gray(data)
+# keypoint, descriptor = features(data, extractor)
+# histogram = build_histogram(descriptor, kmeans)
+# neighbor = NearestNeighbors(n_neighbors = 20)
+# neighbor.fit(preprocess_image)
+# dist, result = neighbor.kneighbors([histogram])
