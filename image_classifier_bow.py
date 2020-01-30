6365786adfc54e842c6502a02540a82ecf6947ee
danilofernandes45 <danilofernandes45@gmail.com>
2020-01-29 22:28:53 -0300

    Ready to test

--- image_classifier_bow.py
+++ image_classifier_bow.py
@@ -12,58 +12,125 @@ def features(image, extractor):
     keypoints, descriptors = extractor.detectAndCompute(image, None)
     return keypoints, descriptors
 
-descriptor_list = np.array( [[ 0 for i in range(128)]] )
-num_descrip = []
+def handOut():
+    descriptor_list = np.array( [[ 0 for i in range(128)]] )
+    num_descrip = []
 
-for tag in ["Full", "Empty"]:
-    for i in range(1, 41):
-        image = cv2.imread("Dataset/"+tag+"/img"+str(i)+".jpg", 0)
-        descriptors = features(image, extractor)[1]
-        descriptor_list = np.append(descriptor_list, descriptors, axis = 0)
-        num_descrip.append( descriptors.shape[0] )
+    for tag in ["Full", "Empty"]:
+        for i in range(1, 41):
+            image = cv2.imread("Dataset/"+tag+"/img"+str(i)+".png", 0)
+            descriptors = features(image, extractor)[1]
+            descriptor_list = np.append(descriptor_list, descriptors, axis = 0)
+            num_descrip.append( descriptors.shape[0] )
 
-descriptor_list = np.delete(descriptor_list, 0, axis = 0)
+    descriptor_list = np.delete(descriptor_list, 0, axis = 0)
 
-# image = cv2.imread("Dataset/Full/img2.jpg", 0)
-# print(features(image, extractor)[1][0])
-# print(descriptor_list[8343])
+    # image = cv2.imread("Dataset/Full/img2.jpg", 0)
+    # print(features(image, extractor)[1][0])
+    # print(descriptor_list[8343])
 
-kmeans = KMeans(n_clusters = n_clusters)
-kmeans.fit( descriptor_list )
+    kmeans = KMeans(n_clusters = n_clusters)
+    kmeans.fit( descriptor_list )
 
-print(kmeans.labels_[0:10])
+    print(kmeans.labels_[0:10])
 
-hist_images = []
-idx = 0
-for num in num_descrip:
-    hist = np.zeros( n_clusters )
-    for k in range(num):
-        hist[kmeans.labels_[idx]] += 1
-        idx += 1
-    norm_hist = hist / num
-    hist_images.append( norm_hist )
-
-hist_images = np.array(hist_images)
-classes = np.zeros(80)
-classes[40:81] = 1
-
-classifier = svm.SVC()
-classifier.fit(hist_images, classes)
-
-#Test phase
-hit = 0
-for tag in ["Full", "Empty"]:
-    for i in range(41, 51):
-        image = cv2.imread("Dataset/"+tag+"/img"+str(i)+".jpg", 0)
-        descriptors = features(image, extractor)[1]
+    hist_images = []
+    idx = 0
+    for num in num_descrip:
         hist = np.zeros( n_clusters )
-        for k in range( len( descriptors ) ):
-            hist[ kmeans.predict( [ descriptors[k] ] ) ] += 1
+        for k in range(num):
+            hist[kmeans.labels_[idx]] += 1
+            idx += 1
+        norm_hist = hist / num
+        hist_images.append( norm_hist )
 
-        norm_hist = hist / len(descriptors)
+    hist_images = np.array(hist_images)
+    classes = np.zeros(80)
+    classes[40:81] = 1
+
+    classifier = svm.SVC()
+    classifier.fit(hist_images, classes)
+
+    #Test phase
+    hit = 0
+    for tag in ["Full", "Empty"]:
+        for i in range(41, 53):
+            image = cv2.imread("Dataset/"+tag+"/img"+str(i)+".png", 0)
+            descriptors = features(image, extractor)[1]
+            hist = np.zeros( n_clusters )
+            for k in range( len( descriptors ) ):
+                hist[ kmeans.predict( [ descriptors[k] ] ) ] += 1
+
+            norm_hist = hist / len(descriptors)
+            prediction = classifier.predict([norm_hist])[0]
+
+            if( tag == "Full" and prediction == 0 or tag == "Empty" and prediction == 1 ):
+                 hit += 1
+
+    print(hit)
+
+def leaveOneOut():
+
+    total_descriptor_list = np.array( [[ 0 for i in range(128)]] )
+    total_start_descrip = [0]
+    total_num_descrip = []
+
+    for tag in ["Full", "Empty"]:
+        for i in range(1, 53):
+            image = cv2.imread("Dataset/"+tag+"/img"+str(i)+".png", 0)
+            descriptors = features(image, extractor)[1]
+            total_descriptor_list = np.append(total_descriptor_list, descriptors, axis = 0)
+            total_num_descrip.append( descriptors.shape[0] )
+            total_start_descrip.append( total_start_descrip[-1] + descriptors.shape[0] )
+
+    total_descriptor_list = np.delete(total_descriptor_list, 0, axis = 0)
+    total_num_descrip = np.array(total_num_descrip)
+    total_start_descrip = np.array(total_start_descrip)
+
+    total_classes = np.zeros(104)
+    total_classes[52:104] = 1
+
+    hit = 0
+
+    for i in range( total_num_descrip.shape[0] ):
+
+        begin = total_start_descrip[i]
+        end = begin + total_num_descrip[i]
+        descriptor_list = np.delete(total_descriptor_list, np.s_[ begin: end ], axis = 0)
+        num_descrip = np.delete(total_num_descrip, i, axis = 0)
+
+        kmeans = KMeans(n_clusters = n_clusters)
+        kmeans.fit( descriptor_list )
+
+        hist_images = []
+        idx = 0
+        for num in num_descrip:
+            hist = np.zeros( n_clusters )
+            for k in range(num):
+                hist[kmeans.labels_[idx]] += 1
+                idx += 1
+            norm_hist = hist / num
+            hist_images.append( norm_hist )
+
+        hist_images = np.array(hist_images)
+
+        classes = np.delete(total_classes, i, axis = 0)
+
+        classifier = svm.SVC()
+        classifier.fit(hist_images, classes)
+
+        hist = np.zeros( n_clusters )
+        for k in range( total_num_descrip[i] ):
+            hist[ kmeans.predict( [ total_descriptor_list[begin + k] ] ) ] += 1
+
+        norm_hist = hist / total_num_descrip[i]
         prediction = classifier.predict([norm_hist])[0]
 
-        if( tag == "Full" and prediction == 0 or tag == "Empty" and prediction == 1 ):
+        if( i < 52 and prediction == 0 or i >= 52 and prediction == 1 ):
              hit += 1
 
-print(hit)
+        print(i)
+
+    print(hit)
+
+leaveOneOut()
